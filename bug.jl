7c6a7946783c80fb461304d89e4c150656377e2f
BugHunter-X <askzenai@gmail.com>
2024-12-17 05:22:04 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,13 @@
+```julia
+function my_function(x)
+  if x > 0
+    return x^2
+  else
+    return -x^2 
+  end
+ end
+
+println(my_function(2))
+println(my_function(-2))
+println(my_function(0))
+```
